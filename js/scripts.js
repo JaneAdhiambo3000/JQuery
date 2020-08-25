8a26d92cf6c7e8d9959e5b4b64718a5b8039d09f
Moringa School Access <shebaz3000@gmail.com>
2020-08-25 18:22:31 +0300

    modified js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,10 @@ $(document).ready(function(){
         $(".walrus-hidden").toggle();
     });
 });
+
+$(document).ready(function(){
+    $(".clickable").click(function(){
+        $(".walrus-showing").toggle();
+        $(".walrus-hidden").toggle();
+    });
+});
